fc1bbf0dc6fd761f0edf654a048237411a14ad02
Gabriel <gabrielblandy@gmail.com>
2022-12-25 14:38:52 -0300

    SpringAPI2 Aula 06

---
+++ spring-boot-admin/src/test/java/com/blandygbc/springbootadmin/SpringBootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blandygbc.springbootadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
