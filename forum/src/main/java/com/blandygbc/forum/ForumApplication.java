c8461f7b28348369e9c6fb323a88bf38705b56ac
Gabriel <gabrielblandy@gmail.com>
2022-12-27 17:40:38 -0300

    SpringAPI3 Alula 05

--- forum/src/main/java/com/blandygbc/forum/ForumApplication.java
+++ forum/src/main/java/com/blandygbc/forum/ForumApplication.java
@@ -2,24 +2,16 @@ package com.blandygbc.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
 @EnableCaching
-public class ForumApplication extends SpringBootServletInitializer {
+public class ForumApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
-	}
-
-	// Configuração para buildar o WAR
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(ForumApplication.class);
 	}
 
 }
